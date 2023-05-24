fc1beb6a9365698cbec153a6a0efb0a0629174f4
Maxim <maxim.kliakhin@gmail.com>
2023-05-24 14:00:20 +0300

    +

--- views/home.php
+++ views/home.php
@@ -28,7 +28,7 @@
     </div>
 </div>
 
-<div class="bestseller-title">
+<div class="block-title">
     Bestseller
 </div>
 <div class="bestseller-slider">
@@ -124,4 +124,175 @@
             <button>></button>
         </div>
     </div>
+</div>
+<div class="hot-category-block">
+    <div class="block-title">
+        Hot Category
+    </div>
+    <div class="hot-category">
+        <div class="discount">
+            <div class="nav-btn">
+                <button>
+                    <img src="views/img/svg/arrow/left.svg" alt="">
+                </button>
+                <button>
+                    <img src="views/img/svg/arrow/right.svg" alt="">
+                </button>
+            </div>
+            <div class="discount-description">
+                <p class="text">20% discount on children's toys</p>
+                <p class="title">More And Higher Discounts On Toys</p>
+                <button>To Shop</button>
+            </div>
+        </div>
+        <div class="hot-categories">
+            <div class="single-hot-category">
+                    <div class="hot-category-text"> 
+                        <p>Smartphones</p>
+                        <a href="#">View More →</a>
+                    </div>
+                    <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+            </div>
+            <div class="single-hot-category">
+                    <div class="hot-category-text"> 
+                        <p>Smartphones</p>
+                        <a href="#">View More →</a>
+                    </div>
+                    <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+            </div>
+            <div class="single-hot-category">
+                    <div class="hot-category-text"> 
+                        <p>Smartphones</p>
+                        <a href="#">View More →</a>
+                    </div>
+                    <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+            </div>
+            <div class="single-hot-category">
+                    <div class="hot-category-text"> 
+                        <p>Smartphones</p>
+                        <a href="#">View More →</a>
+                    </div>
+                    <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+            </div>
+            <div class="single-hot-category">
+                    <div class="hot-category-text"> 
+                        <p>Smartphones</p>
+                        <a href="#">View More →</a>
+                    </div>
+                    <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+            </div>
+            <div class="single-hot-category">
+                    <div class="hot-category-text"> 
+                        <p>Smartphones</p>
+                        <a href="#">View More →</a>
+                    </div>
+                    <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+            </div>
+        </div>
+    </div>      
+    <div class="why-we">
+        <div class="why-list">
+            <p class="why-num">1.</p>
+            <div class="why-description">
+                <p class="why-title">Big Choice</p>
+                <p class="why-sub-title">Products for everyone.</p>
+            </div>
+        </div>
+        <div class="why-list">
+            <p class="why-num">2.</p>
+            <div class="why-description">
+                <p class="why-title">Big Choice</p>
+                <p class="why-sub-title">Products for everyone.</p>
+            </div>
+        </div>
+        <div class="why-list">
+            <p class="why-num">3.</p>
+            <div class="why-description">
+                <p class="why-title">Big Choice</p>
+                <p class="why-sub-title">Products for everyone.</p>
+            </div>
+        </div>
+        <div class="why-list">
+            <p class="why-num">4.</p>
+            <div class="why-description">
+                <p class="why-title">Big Choice</p>
+                <p class="why-sub-title">Products for everyone.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="recomend-product-block">
+    <div class="notice-block">
+        <div class="catch-discount">
+            <p>Shoes discount</p>
+            <p>Catch A Discount</p>
+            <div class="counter">
+                <div class="days">
+                    <p>02</p>
+                    <p>DAYS</p>
+                </div>
+                <div class="hours">
+                    <p>12</p>
+                    <p>HR</p>
+                </div>
+                <div class="min">
+                    <p>32</p>
+                    <p>MIN</p>
+                </div>
+                <div class="sec">
+                    <p>54</p>
+                    <p>SC</p>
+                </div>
+            </div>
+            <button class="read-more-btn">Read More</button>
+        </div>
+        <div class="new-collection">
+            <p>For your home</p>
+            <p>New Decore C</p>
+        </div>
+    </div>
+    <div class="recomend-products">
+
+        <div class="recomend-products-title">
+            <div class="block-title">Recomend products</div>
+            <div class="product-sort">
+            <div class="bestsellers">
+            <div class="bestsellers-product">
+                <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+                <p>Samsung Galaxy S20 Ultra</p>
+                <p class="price">$ 1300.00</p>
+            </div>
+            <div class="bestsellers-product">
+                <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+                <p>Samsung Galaxy S20 Ultra</p>
+                <p class="price">$ 1300.00</p>
+            </div>
+            <div class="bestsellers-product">
+                <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+                <p>Samsung Galaxy S20 Ultra</p>
+                <p class="price">$ 1300.00</p>
+            </div>
+            <div class="bestsellers-product">
+                <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+                <p>Samsung Galaxy S20 Ultra</p>
+                <p class="price">$ 1300.00</p>
+            </div>
+            <div class="bestsellers-product">
+                <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+                <p>Samsung Galaxy S20 Ultra</p>
+                <p class="price">$ 1300.00</p>
+            </div>
+            <div class="bestsellers-product">
+                <img src="views/img/products/hyperion-product-img-1.jpg" alt="">
+                <p>Samsung Galaxy S20 Ultra</p>
+                <p class="price">$ 1300.00</p>
+            </div>
+        </div>
+            </div>
+        </div>
+    </div>
+
+
 </div>
